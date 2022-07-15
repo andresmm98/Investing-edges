d73166cd2ff9ce9db3192b75d7e2af890c2bd209
Anmar <andresmm1998@gmail.com>
2022-07-15 18:47:43 +0200

    Conseguida primera versión funcional Añadida una función en market_data llamada get que obtiene los datos solicitados para unos tickers y fechas concretas

--- code/backtesting.py
+++ code/backtesting.py
@@ -18,7 +18,7 @@
 
 import market_data # esta sería nuestro programa que nos da los datos a partir de la API
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime, timedelta
 import logging
@@ -52,7 +52,7 @@ class strategy(object):
         # - 3 columnas de nombre "symbol", "factor" y "open"
         # "close" es solamente el precio de cierre en esa fecha
 
-        stocks_df = market_data.create_dataframe(date, self.factor)
+        stocks_df = market_data.get(date, ['open', self.factor])
         
         stocks_df.sort_values(by=self.factor, ascending=self.less_is_better)
 
@@ -62,11 +62,13 @@ class strategy(object):
         # Como la división de acciones por cuantil no será siempre entera, ponemos una acción más en los primeros cuantiles hasta que el resto sea 0
         remainder = n_stocks % n_quantiles
         split_indexes = []
-        for i in range(n_quantiles):
+        index = 0
+        for i in range(n_quantiles-1):
             if i < remainder:
-                split_indexes.append(stocks_per_quantile + i + 1)
+                index += stocks_per_quantile + 1
             else:
-                split_indexes.append(stocks_per_quantile + remainder)
+                index += stocks_per_quantile 
+            split_indexes.append(index)
 
         # Divide el datafrme en {quantiles} y los mete en una lista
         quantile_list = np.split(stocks_df, split_indexes)
@@ -74,13 +76,14 @@ class strategy(object):
         return quantile_list
     
     def backtest(self, 
-                n_quantiles=10,
+                n_quantiles=4,
                 market='NYSE',
-                n_stocks=100,
-                start_date='2018-03-09',
+                n_stocks=200,
+                start_date='2019-03-09',
                 check_period=30, # Periodo para actualizar el valor de la cartera. De momento debe estar en días
                 rebalance_period=90, # Debe ser un múltiplo de {check_period}
                 budget=100000): # presupuesto por portfolio
+        
         ''' Hace el backtesting de la estrategia dada 
         
         La lógica de la función es la siguiente:
@@ -89,13 +92,13 @@ class strategy(object):
         
         Los cuantiles vuelven a calcularse para cada nuevo rebalanceo'''
         
-        # Initialize market data
         logging.debug("Importing market data...")
         mk = market_data.market_data(market, n_stocks)
         logging.info("Market data imported succesfully.")
         
         date = datetime.strptime(start_date, '%Y-%m-%d')
-        n_checks = rebalance_period / check_period
+        n_checks = int(rebalance_period / check_period)
+        rebalanced_dates = []
  
         # initialize portfolios
         portfolio_list = []
@@ -107,44 +110,56 @@ class strategy(object):
             # rebalance portfolios
             quantile_list = self.create_quantiles(mk, n_quantiles, date.strftime('%Y-%m-%d'))
             logging.info(f"Stocks divided in {n_quantiles}")
+            rebalanced_dates.append(date.strftime('%m/%y'))
             
             for i in range(n_quantiles):
-                portfolio_list[i] = portfolio_list[i].build_portfolio(quantile_list[i]['tickers'], quantile_list[i]['prices'], date.strftime('%Y-%m-%d'))
+                quantile_list[i] = quantile_list[i].reset_index(drop=True)
+                portfolio_list[i].build(date.strftime('%Y-%m-%d'), quantile_list[i]['tickers'], quantile_list[i]['open'])
                 logging.info(f"Bought stocks at {date} for portfolio nº {i}")
 
-                # update performance
-                for j in range(n_checks):
-                    date += timedelta(check_period)
-                    for j in range(n_quantiles):
+            # update performance
+            for j in range(n_checks):
+                date += timedelta(check_period)
+                if date < datetime.today():
+                    for i in range(n_quantiles):
                         portfolio_list[i].compute_performance(mk, date.strftime('%Y-%m-%d'))
                 
-                logging.info(f"Updated performance for all portfolios at date {date}")
+                    logging.info(f"Updated performance for all portfolios at date {date}")
 
-            logging.info(f"Finished backtesting. Returning portfolio performances...")
+        logging.info(f"Finished backtesting. Returning portfolio performances...")
         
         # plot
-        '''
+        if self.less_is_better == True:
+            ind = 'lowest' 
+        else:
+            ind = 'highest'
+        
         fig = plt.figure(figsize=(16,32))
         ax = fig.add_subplot()
         for i in range(len(portfolio_list)):
-            ax.plot([x[2] for x in portfolio_list[i].performace])
+            ax.plot([x[1] for x in portfolio_list[i].performance], label = f'{i+1}º quantil')
         plt.legend(loc='upper right')
-        plt.ylabel('Portfolio Value')
-        plt.title(f'{n_quantiles} portfolios by {self.factor}')
-        plt.xlabel('date',labelpad=12)
+        plt.ylabel('Returns (%)')
+        plt.suptitle(f'Returns of {n_quantiles} portfolios by {ind} {self.factor}')
+        plt.title(f'market = {market}, rebalance after {rebalance_period} days')
+        plt.xlabel('date')
+        plt.xticks([i for i in range(len(portfolio_list[i].performance)) if i%3 == 0], rebalanced_dates)
         ax.yaxis.tick_right()
-        plt.show()'''
+        plt.show()
         
         return portfolio_list
 
 class portfolio():
 
-    def __init__(self, start_date, initial_budget):
+    def __init__(self, start_date, budget):
 
         self.positions = []
-        self.perfomance = [(start_date, initial_budget)] # lista de tuplas con fecha y valor
+        self.initial_budget = budget
+        self.value = budget
+        self.performance = [(start_date, 100)] # lista de tuplas con fecha y valor
+        self.last_rebalance = None
 
-    def build_portfolio(self, date, budget, tickers, prices):
+    def build(self, date, tickers, prices):
         ''' Dado un cuantil de acciones se construye un portfolio para el presupuesto actual.
         El presupuesto se divide por igual entre todas las acciones.'''
 
@@ -152,39 +167,39 @@ class portfolio():
         
         n_stocks = len(tickers)
         
-        money_per_stock = self.curr_value / n_stocks
+        money_per_stock = self.value / n_stocks
 
         for i in range(n_stocks):
-            position = money_per_stock / prices[i]
-            self.positions.append((tickers[i], float(f'(position:4f)'))) # cada posición tiene 4 decimales, ignoramos el error de truncamiento
+            n_decimals = 10000 # 4 decimales, ignoramos error de truncamiento
+            position = int(money_per_stock / prices[i] * n_decimals)
+            self.positions.append((tickers[i], float(position)/n_decimals)) 
 
         self.last_rebalance = date # fecha del último rebalanceo
-        
-        return self
 
     def compute_performance(self, market_data, date):
         ''' De momento solo calcula el valor de un portfolio para una fecha X '''
 
         portfolio_value = 0
-        tickers = (i for i, j in self.positions)
-        shares = (j for i, j in self.positions)
+        tickers = [i for i, j in self.positions]
+        shares = [j for i, j in self.positions]
         
         # Se espera una lista con los precios de cierre del día anterior. Si la acción ha sido deslistada debe devolver None
-        prices = market_data.get_prices(tickers, date)
+        mk = market_data.get(date, ['close'], tickers)
+        prices = mk['close']
 
         for i in range(len(tickers)):
             # suma el valor actual de las posiciones
-            if prices[i] is not None:
+            portfolio_value += shares[i] * prices[i]
+            '''if prices[i] is not None:
                 portfolio_value += shares[i] * prices[i]
             else:
-                portfolio_value += shares[i] * market_data.get_delisting_price[tickers[i]] # buscar el precio en fecha de delisting
+                portfolio_value += shares[i] * market_data.get_delisting_price[tickers[i]] # buscar el precio en fecha de delisting'''
 
             # suma los dividendos pagados
-            portfolio_value += market_data.get_paid_dividends(tickers[i], date - self.backtesting.check_period, date) # debe calcular los dividendos pagados por una lista de acciones entre dos fechas
+            # portfolio_value += market_data.get_paid_dividends(tickers[i], date - self.backtesting.check_period, date) # debe calcular los dividendos pagados por una lista de acciones entre dos fechas
 
-        self.performance.append(date, portfolio_value)
-
-        return self
+        self.value = portfolio_value
+        self.performance.append((date, int(self.value/self.initial_budget*100)))
 
 def main():
     with strategy() as factor:
--- code/market_data.py
+++ code/market_data.py
@@ -6,6 +6,8 @@
 from numpy import empty
 import requests 
 import pandas as pd 
+from datetime import datetime, timedelta
+import json
 
 def req(request_url, api_key):
     base_url = f'https://financialmodelingprep.com/api/v3/'
@@ -57,7 +59,7 @@ class market_data():
             
         return val
 
-    def create_dataframe(self,date,factor): 
+    def create_dataframe(self,date_str,factor): 
 
         # Crea un df de tres columnas (ticker, apertura y factor) a partir de la base de datos (para una fecha dada)
 
@@ -66,14 +68,31 @@ class market_data():
         
         # Obten las cotizaciones a la apertura de esos tickers en esa fecha:
         # Si no se puede obtener la cotización, inseta un []
+        tickers2 = []
         opens = []
+        factors = []
+        date = datetime.strptime(date_str, '%Y-%m-%d')
         for i in range(len(tickers)):
+            first_date = self.dta[i]['historical'][-1]['date']
+            if datetime.strptime(first_date, '%Y-%m-%d') < date:
+                day = next((e for e in self.dta[i]['historical'] if datetime.strptime(e['date'], '%Y-%m-%d') <= date), None)                
+                try:
+                    factors.append(day[f'{factor}'])
+                    tickers2.append(tickers[i])
+                    opens.append(day['open'])
+                except: pass
+                
+        '''while opens.count(None) == len(opens):
+            for i in range(len(tickers)):
 
-            try:
-                o = self.get_dta(date,tickers[i],'open')
-            except:
-                o = None
-            opens.append(o)
+                try:
+                    o = self.get_dta(date_str,tickers[i],'open')
+                except:
+                    o = None
+                opens.append(o)
+            date = datetime.strptime(date_str, '%Y-%m-%d')
+            date -= timedelta(1)
+            date_str = date.strftime('%Y-%m-%d')
 
         # Obten el factor para cada ticker en esa fecha
         # Si no se puede obtener el factor, inseta un []
@@ -83,11 +102,61 @@ class market_data():
                f = self.get_dta(date,tickers[i],f'{factor}')
             except:
                 f = None
-            factors.append(f)
+            factors.append(f)'''
 
         # Crea el df:
-        return pd.DataFrame({'Tickers': tickers, 'Opens': opens, 'Factor':factors}).dropna() #factor
+        return pd.DataFrame({'tickers': tickers2, 'opens': opens, 'factor':factors})#.dropna() #factor
 
+    def get(self, date_str, data_points, tickers=[]):
+        # si no le das tickers coge todos
+        if not tickers:
+            tickers = self.symbols
+        
+        # inicializa listas para cada variable pedida
+        requested_data = [[]]
+        n_points = len(data_points)
+        for i in range(n_points):
+            requested_data.append([])
+
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+
+        for i in range(len(tickers)):
+            
+            day = None
+            
+            if len(tickers) == len(self.symbols):
+                
+                # si la fecha pedida es anterior a la primera de la empresa, ya sabemos que no sirve
+                first_date = self.dta[i]['historical'][-1]['date'] 
+                if datetime.strptime(first_date, '%Y-%m-%d') < date:
+
+                    # cogemos la fecha más reciente que sea igual o anterior a la pedida. Sirve para delistings
+                    day = next((e for e in self.dta[i]['historical'] if datetime.strptime(e['date'], '%Y-%m-%d') <= date), None)   
+            
+            else:
+
+                historical = next((e['historical'] for e in self.dta if e['symbol'] == tickers[i]), None)
+                day = next((e for e in historical if datetime.strptime(e['date'], '%Y-%m-%d') <= date), None)
+            
+            if day is not None:
+                requested_data[0].append(tickers[i])
+                
+                # si alguno de los datos pedidos para la empresa está vacío, la elimina de la lista
+                try:
+                    for j in range(n_points):
+                        requested_data[j+1].append(day[f'{data_points[j]}'])
+                except: 
+                    while j >= 0:
+                        requested_data[j].pop()
+                        j -= 1
+        
+        data_dict = {'tickers':requested_data[0]}
+        for i in range (n_points):
+            dict = {data_points[i]:requested_data[i+1]}
+            data_dict.update(dict)
+
+        return pd.DataFrame(data_dict)
+    
     def get_prices(self,tickers,date):
 
         # Devuelve una lista de los cierres 
